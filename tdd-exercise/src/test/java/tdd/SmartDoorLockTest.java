b2922ef9d759ea08d4056057207b3d010354bd05
Norbert Gabos <dont.vip@gmail.com>
2025-02-24 11:22:20 +0100

    create DoorLock

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,21 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    private SmartDoorLock doorLock;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.doorLock = new DoorLock();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testLockDoor() {
+
     }
 }
